5f05c826dc70d6e5ce07eb9330485ec7f0f1d43d
Ksenia <83863785+DowDL@users.noreply.github.com>
2022-01-23 22:43:31 +0100

    Fix prefabs scale

--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -4,29 +4,21 @@ using UnityEngine;
 
 public class RandomSpawner : MonoBehaviour
 {
-    //public GameObject Donut_1;
-    //public GameObject Donut_2;
-    //public GameObject Donut_3;
-    //public GameObject Donut_4;
+   
     public List<GameObject> Donuts = new List<GameObject>();
 
     private int Donuts_count = 1;
-    public float Radius = 1.0f;
+    public float Length = 1.0f;
+    public float Width = 1.0f;
+    public float Height = 1.0f;
     public float TimeLeft = 60.0f;
     public float Delay = 1f;
     public float Timer;
 
-    //void Start()
-   // {
-    //    Donuts.Add(Donut_1);
-    //    Donuts.Add(Donut_2);
-   //     Donuts.Add(Donut_3);
-    //    Donuts.Add(Donut_4);
-   // }
-
+   
     void Update()
     {
-        if(Timer <= 0)
+        if (Timer <= 0)
         {
             SpawnObjectAtRandom(Donuts[Random.Range(0, Donuts.Count)]);
             Timer = Delay;
@@ -34,36 +26,17 @@ public class RandomSpawner : MonoBehaviour
         Timer -= Time.deltaTime;
 
         return;
-/*
-        if (TimeLeft >= 0)
-        {
-            switch (Donuts_count)
-        {
-            case 1:
-                SpawnObjectAtRandom(Donut_1);
-                break;
-            case 2:
-                SpawnObjectAtRandom(Donut_2);
-                break;
-            case 3:
-                SpawnObjectAtRandom(Donut_3);
-                break;
-            case 4:
-                {
-                SpawnObjectAtRandom(Donut_4);
-                Donuts_count = 0;
-                }
-                break;
-        }
-        TimeLeft -= Time.deltaTime;
-            ++Donuts_count;
-        } 
-*/
+       
     }
 
     void SpawnObjectAtRandom(GameObject donut)
     {
-        Vector3 randomPos = Random.insideUnitSphere * Radius;
+
+        Vector3 randomPos = new Vector3(
+                   (Random.value - 0.5f) * Length,
+                   (Random.value - 0.5f) * Height,
+                   (Random.value - 0.5f) * Width
+                   );
 
         Instantiate(donut, this.transform.position + randomPos, Quaternion.identity);
     }
@@ -72,6 +45,8 @@ public class RandomSpawner : MonoBehaviour
     {
         Gizmos.color = Color.red;
 
-        Gizmos.DrawWireSphere(this.transform.position, Radius);
+        Vector3 size = new Vector3(Length, Height, Width);
+
+        Gizmos.DrawWireCube(this.transform.position, size);
     }
 }
